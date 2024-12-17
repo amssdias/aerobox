cddb97f29045320f9292e42d27647847a0a77b3b
Dias <andremssdias@hotmail.com>
2024-12-17 23:04:43 +0100

    feat(cloud_storage): Update CloudFile validation serializer

--- apps/cloud_storage/serializers/cloud_files.py
+++ apps/cloud_storage/serializers/cloud_files.py
@@ -1,10 +1,14 @@
-from apps.cloud_storage.models import CloudFiles
+import mimetypes
+
+from apps.cloud_storage.models import CloudFile
 from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
+
 
 
 class CloudFilesSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CloudFiles
+        model = CloudFile
         fields = (
             "name",
             "path",
@@ -15,3 +19,23 @@ class CloudFilesSerializer(serializers.ModelSerializer):
             "checksum",
             "file_url",
         )
+
+    def validate(self, data):
+        user = self.context["request"].user
+        if not user:
+            raise serializers.ValidationError(_("User is not authenticated."))
+
+        file_name = data.get("file_name")
+        content_type = mimetypes.guess_type(file_name)
+        if content_type != data.get("file_type"):
+            raise serializers.ValidationError(_("Content type sent is not correct."))
+
+        return data
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        validated_data["user"] = user
+
+        file_name = validated_data.get("file_name")
+        validated_data["file_type"] = mimetypes.guess_type(file_name)
+        return super().create(validated_data)
