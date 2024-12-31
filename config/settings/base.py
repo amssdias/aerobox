4a62e333468aa23a8ecc6cce054c39367b9cc7a2
Dias <andremssdias@hotmail.com>
2024-12-31 17:07:55 +0100

    config: Use postgres DB

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT", "3306"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
