c842e2b43adc5f326b5034f28ca2149e5c0ec159
Dias <andremssdias@hotmail.com>
2025-01-09 20:33:42 +0000

    feat(cloud_storage): Add file validation if exists on S3

--- apps/cloud_storage/services/s3_service.py
+++ apps/cloud_storage/services/s3_service.py
@@ -42,9 +42,13 @@ class S3Service:
         :param bucket_name: Name of the S3 bucket
         :param object_name: S3 key (file path) in the bucket
         :param expiration: Time in seconds for the presigned URL to remain valid
-        :return: Presigned URL as a string or None if there is an error
+        :return: Presigned URL as a string or None if the file does not exist or there is an error
         """
         try:
+
+            # Check if the file exists first
+            self.s3_client.head_object(Bucket=bucket_name, Key=object_name)
+
             presigned_url = self.s3_client.generate_presigned_url(
                 ClientMethod="get_object",
                 Params={
@@ -54,6 +58,16 @@ class S3Service:
                 ExpiresIn=expiration,
             )
             return presigned_url
-        except (NoCredentialsError, ClientError) as e:
-            print(f"Error generating presigned download URL: {e}")
+
+        except ClientError as e:
+
+            if e.response["Error"]["Code"] == "404":
+                print(f"File '{object_name}' not found in S3 bucket '{bucket_name}'.")
+                return None
+            else:
+                print(f"Error generating presigned download URL: {e}")
+                return None
+
+        except NoCredentialsError:
+            print("AWS credentials not found.")
             return None
