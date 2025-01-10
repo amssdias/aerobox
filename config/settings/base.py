d16dee7f759819af852750dccaa2090a67a75b7c
Dias <andremssdias@hotmail.com>
2025-01-10 00:42:56 +0000

    config: Create default pagination

--- config/settings/base.py
+++ config/settings/base.py
@@ -146,6 +146,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 25,
 }
 
 # Schema API
