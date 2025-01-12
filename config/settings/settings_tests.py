28a497d44db03d757ac890d1b7e7d5683a6eba7b
Dias <andremssdias@hotmail.com>
2025-01-12 14:36:18 +0100

    config: Add settings tests variables on AWS s3

--- config/settings/settings_tests.py
+++ config/settings/settings_tests.py
@@ -11,3 +11,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# AWS S3
+AWS_STORAGE_BUCKET_NAME = "test-bucket"
+AWS_S3_BASE_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
