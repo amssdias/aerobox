59b4712cbb560ed576102af31fbddc84c3d9918f
Dias <andremssdias@hotmail.com>
2025-01-13 11:54:45 +0000

    config: Create settings tests logging

--- config/settings/settings_tests.py
+++ config/settings/settings_tests.py
@@ -12,6 +12,29 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,  # Prevents re-initializing existing loggers
+    "handlers": {
+        "null": {
+            "class": "logging.NullHandler",  # Completely swallows logs
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["null"],  # Swallow all Django logs
+            "level": "CRITICAL",   # Ignore anything below CRITICAL
+            "propagate": False,    # Prevent logs from bubbling up
+        },
+        "aerobox":  {
+            "handlers": ["null"],
+            "level": "CRITICAL",
+            "propagate": False,
+        }
+    },
+}
+
+
 # AWS S3
 AWS_STORAGE_BUCKET_NAME = "test-bucket"
 AWS_S3_BASE_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
