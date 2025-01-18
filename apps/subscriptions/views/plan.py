1daa3ee0b80cb606fc4c7c133c3a4f47e0db64a1
Dias <andremssdias@hotmail.com>
2025-01-18 11:48:13 +0000

    Docs(subscriptions): Plan api list view.

--- apps/subscriptions/views/plan.py
+++ apps/subscriptions/views/plan.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
 
@@ -5,9 +6,9 @@ from apps.subscriptions.models import Plan
 from apps.subscriptions.serializers.plan import PlanSerializer
 
 
+@extend_schema(tags=["API - Subscriptions"])
 class PlanListAPIView(ListAPIView):
     queryset = Plan.objects.filter(is_active=True)
     serializer_class = PlanSerializer
     permission_classes = [AllowAny]
     pagination_class = None
-
