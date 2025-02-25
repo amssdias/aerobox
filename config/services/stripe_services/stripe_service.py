7bfbc8f4e6bd40f8df6a7b7925d2a245197b1b9a
Dias <andremssdias@hotmail.com>
2025-02-25 23:24:46 +0100

    Config(stripe): Create stripe service to route events handlers.

---
+++ config/services/stripe_services/stripe_service.py
@@ -0,0 +1,41 @@
+import logging
+
+import stripe
+from django.conf import settings
+
+from apps.subscriptions.services.stripe_events.stripe_subscription import (
+    SubscriptiondHandler,
+)
+
+logger = logging.getLogger("aerobox")
+
+
+class StripeService:
+    """
+    A service class for handling Stripe operations.
+    """
+
+    def __init__(self):
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    def process_webhook_event(self, event):
+        """
+        Routes Stripe events to the correct handler.
+        """
+        event_type = event["type"]
+        handler = self.get_handler(event_type, event)
+
+        if handler:
+            handler.process()
+        else:
+            logger.error(f"Unhandled event type: {event}")
+
+    @staticmethod
+    def get_handler(event_type, event):
+        """
+        Returns the correct event handler based on the event type.
+        """
+        handlers = {
+            "customer.subscription.created": SubscriptiondHandler,
+        }
+        return handlers.get(event_type, None)(event) if event_type in handlers else None
