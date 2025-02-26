74f6064d7e2a9f250bf9fbe0ac7ac3cbe5fedbec
Dias <andremssdias@hotmail.com>
2025-02-26 23:43:00 +0100

    Tests(subscriptions): Create Subscription Factory.

---
+++ apps/subscriptions/factories/subscription.py
@@ -0,0 +1,26 @@
+from datetime import timedelta
+
+import factory
+from django.utils.timezone import now
+
+from apps.subscriptions.choices.subscription_choices import (
+    SubscriptionBillingCycleChoices,
+    SubscriptionStatusChoices,
+)
+from apps.subscriptions.factories.plan_factory import PlanFactory
+from apps.subscriptions.models import Subscription
+from apps.users.factories.user_factory import UserFactory
+
+
+class SubscriptionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Subscription
+
+    user = factory.SubFactory(UserFactory)
+    plan = factory.SubFactory(PlanFactory)
+    stripe_subscription_id = factory.Faker("uuid4")
+    billing_cycle = SubscriptionBillingCycleChoices.MONTH.value
+    start_date = factory.LazyFunction(now)
+    end_date = factory.LazyAttribute(lambda obj: obj.start_date + timedelta(days=31))
+    status = SubscriptionStatusChoices.ACTIVE.value
+    is_recurring = True
