f78e72bb85e0c06994e513336f270774ea401911
Dias <andremssdias@hotmail.com>
2025-03-01 12:38:34 +0100

    Migrations(payments): Add null value to payment date and payment method.

--- apps/payments/models/payment.py
+++ apps/payments/models/payment.py
@@ -8,6 +8,8 @@ from apps.payments.choices.payment_choices import (
 )
 from apps.subscriptions.models import Subscription
 from config.models import Timestampable
+from django.utils.translation import gettext_lazy as _
+
 
 
 class Payment(Timestampable):
@@ -18,9 +20,13 @@ class Payment(Timestampable):
         Subscription, on_delete=models.CASCADE, related_name="payments"
     )
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    payment_date = models.DateTimeField()
+    payment_date = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text=_("The date and time when the payment was made."),
+    )
     payment_method = models.CharField(
-        max_length=10, choices=PaymentMethodChoices.choices
+        max_length=10, choices=PaymentMethodChoices.choices, null=True, blank=True,
     )
     status = models.CharField(max_length=20, choices=PaymentStatusChoices.choices)
     currency = models.CharField(
