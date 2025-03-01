30477c0d1605342ae0843cdbdba2298e60a29ed7
Dias <andremssdias@hotmail.com>
2025-03-01 12:32:23 +0100

    refactor(subscriptions): Inherit customers strie pmixin for event handlers.

--- apps/subscriptions/services/stripe_events/stripe_subscription_deleted.py
+++ apps/subscriptions/services/stripe_events/stripe_subscription_deleted.py
@@ -6,25 +6,28 @@ from apps.subscriptions.choices.subscription_choices import (
 )
 from apps.subscriptions.models import Subscription
 from config.services.stripe_services.stripe_events.base_event import StripeEventHandler
+from config.services.stripe_services.stripe_events.customer_event import StripeCustomerMixin
 
 logger = logging.getLogger("aerobox")
 
 
-class SubscriptionDeleteddHandler(StripeEventHandler):
+class SubscriptionDeleteddHandler(StripeEventHandler, StripeCustomerMixin):
     """
     Handles `customer.subscription.deleted` event.
     """
 
     def process(self):
         subscription_id = self.data["id"]
+        subscription = self.get_subscription(subscription_id=subscription_id)
+
+        if not subscription:
+            return
+
         ended_at = self.data.get("ended_at", None)
+        self.update_subscription(subscription, ended_at)
 
-        try:
-            subscription = Subscription.objects.get(stripe_subscription_id=subscription_id)
-
-            subscription.status = SubscriptionStatusChoices.CANCELED.value
-            subscription.end_date = datetime.utcfromtimestamp(ended_at).date() if ended_at else subscription.end_date
-            subscription.save()
-
-        except Subscription.DoesNotExist:
-            logger.error("Subscription does not exist", extra={"stripe_subscription_id": subscription_id})
+    @staticmethod
+    def update_subscription(subscription, ended_at):
+        subscription.status = SubscriptionStatusChoices.CANCELED.value
+        subscription.end_date = datetime.utcfromtimestamp(ended_at).date() if ended_at else subscription.end_date
+        subscription.save()
