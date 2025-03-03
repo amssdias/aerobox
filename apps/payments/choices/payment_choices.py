cd67d4897f2a2c6f27bb696ed3483fa458668cd8
Dias <andremssdias@hotmail.com>
2025-03-03 01:19:06 +0100

    Migrations(payments): Add 'retrying' status for failed auto-payments.

--- apps/payments/choices/payment_choices.py
+++ apps/payments/choices/payment_choices.py
@@ -14,6 +14,7 @@ class PaymentStatusChoices(models.TextChoices):
     PENDING = "pending", _("Pending")
     PAID = "paid", _("Paid")
     CANCELED = "canceled", _("Canceled (Void)")
+    RETRYING = "retrying", _("Retrying")
 
 
 class PaymentCurrenciesChoices(models.TextChoices):
