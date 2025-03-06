57293aecd9ad613c84206fda8e0d952b3b5f3007
Dias <andremssdias@hotmail.com>
2025-03-06 22:53:47 +0100

    Config: Fix env settings.

--- config/settings/base.py
+++ config/settings/base.py
@@ -176,8 +176,8 @@ PASSWORD_RESET_TIMEOUT = 3600 # 1 hour
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
+EMAIL_PORT = os.environ.get("EMAIL_PORT", 587)
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
