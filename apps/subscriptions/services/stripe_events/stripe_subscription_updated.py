2178f26ba7085f1f31b11ed400f9c65c3e9922f8
Dias <andremssdias@hotmail.com>
2025-03-10 23:02:16 +0100

    Feat(subscriptions): Add 'past_due' status to a subscription if user does not pay.

--- apps/subscriptions/services/stripe_events/stripe_subscription_updated.py
+++ apps/subscriptions/services/stripe_events/stripe_subscription_updated.py
@@ -1,8 +1,15 @@
 from apps.subscriptions.choices.subscription_choices import (
     SubscriptionStatusChoices,
 )
+from apps.subscriptions.constants.stripe_subscription_status import (
+    INCOMPLETE,
+    ACTIVE,
+    PAST_DUE,
+)
 from config.services.stripe_services.stripe_events.base_event import StripeEventHandler
-from config.services.stripe_services.stripe_events.customer_event import StripeCustomerMixin
+from config.services.stripe_services.stripe_events.customer_event import (
+    StripeCustomerMixin,
+)
 
 
 class SubscriptionUpdateddHandler(StripeEventHandler, StripeCustomerMixin):
@@ -26,15 +33,21 @@ class SubscriptionUpdateddHandler(StripeEventHandler, StripeCustomerMixin):
     def get_subscription_status(self):
         if "status" not in self.data:
             return None
-        if self.data["status"] == "incomplete":
+
+        # PAST_DUE: When a user's payment fails, the subscription is marked as inactive.
+        if self.data["status"] in [INCOMPLETE, PAST_DUE]:
             return SubscriptionStatusChoices.INACTIVE.value
-        elif self.data["status"] == "active":
+        elif self.data["status"] == ACTIVE:
             return SubscriptionStatusChoices.ACTIVE.value
 
     def can_update(self, subscription, subscription_id, status):
         if not subscription:
-            raise ValueError(f"Subscription with Stripe ID '{subscription_id}' not found.")
+            raise ValueError(
+                f"Subscription with Stripe ID '{subscription_id}' not found."
+            )
         if not status:
-            raise ValueError(f"Subscription status missing in Stripe event: {self.data}")
+            raise ValueError(
+                f"Subscription status missing in Stripe event: {self.data}"
+            )
 
         return True
