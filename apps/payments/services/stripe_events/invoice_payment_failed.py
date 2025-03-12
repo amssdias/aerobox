91368de440baa7df01b3d199f70a754c83b29a02
Dias <andremssdias@hotmail.com>
2025-03-12 22:52:01 +0100

    Feat(payments): Create invoice payment failed event handler.

---
+++ apps/payments/services/stripe_events/invoice_payment_failed.py
@@ -0,0 +1,42 @@
+import logging
+
+from apps.payments.choices.payment_choices import PaymentStatusChoices
+from apps.payments.constants.stripe_invoice import SUBSCRIPTION_CYCLE
+from config.services.stripe_services.stripe_events.base_event import StripeEventHandler
+from config.services.stripe_services.stripe_events.invoice_event_mixin import (
+    StripeInvoiceMixin,
+)
+
+logger = logging.getLogger("aerobox")
+
+
+class InvoicePaymentFailedHandler(StripeEventHandler, StripeInvoiceMixin):
+    """
+    Handles the `invoice.payment_failed` event.
+    """
+
+    def process(self):
+        invoice_id = self.get_invoice_id()
+        payment = self.get_payment(invoice_id)
+
+        billing_reason = self.get_billing_reason()
+
+        if self.is_subscription_cycle(billing_reason):
+            self.update_payment(payment)
+
+    @staticmethod
+    def is_subscription_cycle(billing_reason):
+        return billing_reason == SUBSCRIPTION_CYCLE
+
+    @staticmethod
+    def update_payment(payment):
+        """
+        Updates the payment status to "retrying," which corresponds to the "past_due"
+        status in Stripe. This indicates that the payment attempt has failed, and
+        Stripe will retry the payment based on the configured dunning settings.
+        """
+        payment.status = PaymentStatusChoices.RETRYING.value
+        payment.save()
+        logger.info(
+            f"Payment retrying: Payment ID {payment.id} failed. User ID {payment.user_id} will be retried based on Stripe's dunning settings."
+        )
