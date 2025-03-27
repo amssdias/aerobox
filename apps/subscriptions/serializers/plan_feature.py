1c217fa711d78b7e857afc1fcbd1043688772004
Dias <andremssdias@hotmail.com>
2025-03-27 22:35:03 +0100

    Feat(subscriptions): Add 'code' and 'default metadata' to feature plan serializer.

--- apps/subscriptions/serializers/plan_feature.py
+++ apps/subscriptions/serializers/plan_feature.py
@@ -6,8 +6,10 @@ from apps.subscriptions.models import PlanFeature
 class FeaturePlanSerializer(serializers.ModelSerializer):
     name = serializers.CharField(source="feature.name")  # Pull from Feature model
     description = serializers.CharField(source="feature.description")
+    code = serializers.CharField(source="feature.code")
+    default_metadata = serializers.JSONField(source="feature.metadata")
     metadata = serializers.JSONField()
 
     class Meta:
         model = PlanFeature
-        fields = ["name", "description", "metadata"]
+        fields = ["code", "name", "description", "metadata", "default_metadata"]
