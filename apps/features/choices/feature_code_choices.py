f55496fec4d868a7eb1285e7f447dbfc4c0ef283
Dias <andremssdias@hotmail.com>
2025-03-27 23:22:42 +0100

    Feat(features): Add features to choices.

--- apps/features/choices/feature_code_choices.py
+++ apps/features/choices/feature_code_choices.py
@@ -4,3 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 class FeatureCodeChoices(models.TextChoices):
     CLOUD_STORAGE = "cloud_storage", _("Cloud Storage")
+    FILE_PREVIEW = "file_preview", _("File Preview")
+    FILE_SHARING = "file_sharing", _("File Sharing via Link")
+    FOLDER_CREATION = "folder_creation", _("Folder Creation")
+    BASIC_SUPPORT = "basic_support", _("Basic Email Support")
