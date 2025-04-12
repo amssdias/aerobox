ce8f042e39e2f937ae87147c6baec89700085b6e
Dias <andremssdias@hotmail.com>
2025-04-12 17:47:44 +0200

    Feat(cloud_storage): Create Folder update and delete view.

--- apps/cloud_storage/views/folder.py
+++ apps/cloud_storage/views/folder.py
@@ -1,15 +1,15 @@
+from django.utils.translation import gettext_lazy as _
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from apps.cloud_storage.models import Folder
 from apps.cloud_storage.serializers import FolderSerializer, FolderDetailSerializer
 
 
 @extend_schema_view(
-    update=extend_schema(exclude=True),
     partial_update=extend_schema(exclude=True),
-    destroy=extend_schema(exclude=True)
 )
 @extend_schema(tags=["API - Cloud Storage"])
 class FolderViewSet(viewsets.ModelViewSet):
@@ -37,3 +37,20 @@ class FolderViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return FolderDetailSerializer
         return FolderSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        Deletes a folder if it is empty and not protected.
+
+        A folder is considered empty if it has no subfolders and no files.
+        Deletion is blocked if the folder:
+        - Is marked as protected (`is_protected=True`) -> Not implemented yet
+        - Contains any direct subfolders or files
+        """
+        folder = self.get_object()
+
+        if folder.subfolders.exists():
+            return Response({"detail": _("Cannot delete a folder that contains files or subfolders.")}, status=400)
+
+        folder.delete()
+        return Response(status=204)
