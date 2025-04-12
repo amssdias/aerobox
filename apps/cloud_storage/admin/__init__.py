2238e337da0e4014205cd6472c9f6b2caea45dd7
Dias <andremssdias@hotmail.com>
2025-04-12 17:11:48 +0200

    Feat(cloud_storage): Create Folder admin.

--- apps/cloud_storage/admin/__init__.py
+++ apps/cloud_storage/admin/__init__.py
@@ -1,1 +1,2 @@
 from .cloud_files import *
+from .folders import *
---
+++ apps/cloud_storage/admin/folders.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from apps.cloud_storage.models import Folder
+
+
+@admin.register(Folder)
+class CloudFilesAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "parent",
+        "user",
+    )
+    list_per_page = 25
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+    raw_id_fields = ("user",)
