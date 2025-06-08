f2c7f4cd0c50cc5aaf24ba3e11102a33f930d09c
Dias <andremssdias@hotmail.com>
2025-06-08 16:57:22 +0200

    test(cloud_storage): update folder view.

--- apps/cloud_storage/tests/views/test_folder_view.py
+++ apps/cloud_storage/tests/views/test_folder_view.py
@@ -234,3 +234,60 @@ class FolderViewSetTests(APITestCase):
         self.assertIn(
             "Your current subscription plan does not include the folder creation feature. Please upgrade your plan to access this functionality.",
             response.data["non_field_errors"][0])
+
+    def test_update_folder_name_only(self):
+        folder = FolderFactory(name="Original", user=self.user)
+        url = reverse("folders-detail", kwargs={"pk": folder.pk})
+        response = self.client.put(url, {"name": "Renamed", "parent_id": ""}, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        folder.refresh_from_db()
+        self.assertEqual(folder.name, "Renamed")
+
+    def test_update_folder_parent_only(self):
+        parent = FolderFactory(user=self.user)
+        folder = FolderFactory(name="Child", user=self.user)
+        url = reverse("folders-detail", kwargs={"pk": folder.pk})
+        response = self.client.put(url, {"name": folder.name, "parent_id": parent.id}, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        folder.refresh_from_db()
+        self.assertEqual(folder.parent_id, parent.id)
+
+    def test_update_folder_name_and_parent(self):
+        old_parent = FolderFactory(user=self.user)
+        new_parent = FolderFactory(user=self.user)
+        folder = FolderFactory(name="ToMove", user=self.user, parent=old_parent)
+        url = reverse("folders-detail", kwargs={"pk": folder.pk})
+        response = self.client.put(url, {"name": "MovedAndRenamed", "parent_id": new_parent.id}, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        folder.refresh_from_db()
+        self.assertEqual(folder.name, "MovedAndRenamed")
+        self.assertEqual(folder.parent_id, new_parent.id)
+
+    def test_update_folder_invalid_name_backslash(self):
+        folder = FolderFactory(user=self.user)
+        url = reverse("folders-detail", kwargs={"pk": folder.pk})
+        response = self.client.put(url, {"name": "Invalid\\Name", "parent_id": ""}, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("name", response.data)
+
+    def test_update_folder_invalid_name_trailing_slash(self):
+        folder = FolderFactory(user=self.user)
+        url = reverse("folders-detail", kwargs={"pk": folder.pk})
+        response = self.client.put(url, {"name": "EndsWith/", "parent_id": ""}, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("name", response.data)
+
+    def test_update_folder_to_existing_name_in_same_parent(self):
+        parent = FolderFactory(user=self.user)
+        FolderFactory(name="Existing", user=self.user, parent=parent)
+        target = FolderFactory(name="Renamable", user=self.user, parent=parent)
+        url = reverse("folders-detail", kwargs={"pk": target.pk})
+        response = self.client.put(url, {"name": "Existing", "parent_id": parent.id}, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("non_field_errors", response.data)
