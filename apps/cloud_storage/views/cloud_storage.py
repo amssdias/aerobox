9d249e3c61bb6611d6d4c9f1baab50b329df86b0
Dias <andremssdias@hotmail.com>
2025-06-24 23:00:39 +0200

    feat(cloud_storage): add pagination for api deleted files.

--- apps/cloud_storage/views/cloud_storage.py
+++ apps/cloud_storage/views/cloud_storage.py
@@ -123,5 +123,11 @@ class CloudStorageViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=["get"], url_path="deleted")
     def deleted_files(self, request):
         queryset = self.get_queryset()
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
