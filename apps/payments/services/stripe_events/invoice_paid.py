24cf327b72ba6e5554e92c9bda38e250720f33c4
Dias <andremssdias@hotmail.com>
2025-06-28 00:43:28 +0200

    feat(payments): manually add paid status to invoice.paid event

--- apps/payments/services/stripe_events/invoice_paid.py
+++ apps/payments/services/stripe_events/invoice_paid.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 
 from django.utils import timezone
 
+from apps.payments.choices.payment_choices import PaymentStatusChoices
 from apps.payments.services.stripe_events.invoice_created import InvoiceCreatedHandler
 from apps.payments.tasks.send_invoice_paid_email import send_invoice_payment_success_email
 from apps.subscriptions.choices.subscription_choices import SubscriptionStatusChoices
@@ -24,11 +25,10 @@ class InvoicePaidHandler(StripeEventHandler, StripeInvoiceMixin):
         payment_method = self.get_payment_method(stripe_invoice)
         amount = self.convert_cents_to_euros(stripe_invoice.amount_paid)
         payment_date = self.get_invoice_paid_date(stripe_invoice)
-        status = self.get_invoice_status(stripe_invoice.status)
 
         payment = self.get_or_create_payment(stripe_invoice.id)
-        if self.can_update(stripe_invoice_id, payment, payment_method, amount, status, ):
-            self.update_payment(payment, payment_method, payment_date, status)
+        if self.can_update(stripe_invoice_id, payment, payment_method, amount):
+            self.update_payment(payment, payment_method, payment_date)
             self.update_subscription(payment.subscription)
             self.send_invoice_paid_email(payment=payment)
 
@@ -37,12 +37,11 @@ class InvoicePaidHandler(StripeEventHandler, StripeInvoiceMixin):
         return payment or InvoiceCreatedHandler(event=self.event).handle_payment_creation()
 
     @staticmethod
-    def can_update(invoice_id, payment, payment_method, amount, status):
+    def can_update(invoice_id, payment, payment_method, amount):
         missing_fields = [
             field_name for field_name, value in [
                 ("payment", payment),
                 ("payment_method", payment_method),
-                ("status", status),
             ] if not value
         ]
 
@@ -69,10 +68,10 @@ class InvoicePaidHandler(StripeEventHandler, StripeInvoiceMixin):
         return True
 
     @staticmethod
-    def update_payment(payment, payment_method, payment_date, status):
+    def update_payment(payment, payment_method, payment_date):
         payment.payment_method = payment_method
         payment.payment_date = payment_date
-        payment.status = status
+        payment.status = PaymentStatusChoices.PAID.value
         payment.save(update_fields=["payment_method", "payment_date", "status"])
 
     @staticmethod
