1dd41b6b4c9946b0530403e666fccc0f16d9d0ec
Dias <andremssdias@hotmail.com>
2026-02-28 14:53:06 +0100

    fix(cloud_files): remove uniqueness from path column.

--- apps/cloud_storage/models/cloud_files.py
+++ apps/cloud_storage/models/cloud_files.py
@@ -33,7 +33,6 @@ class CloudFile(Timestampable, SoftDeleteModel):
     )
     path = models.CharField(
         max_length=255,
-        unique=True,
         help_text=_("The path where the file is stored.")
     )
     s3_key = models.CharField(max_length=1024, unique=True, null=True, blank=True, help_text="Full S3 object key path")
